a534a35fba32f65644d67e1b8e3ee56731e70110
Evgenygroup <mailfrom61@gmail.com>
2022-02-16 11:47:58 +0100

    lesson 1

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    int a;
+    int b=10;
+    int A;
+
+
+
+    double c;
+    // type name
+    // name=value
+
+
+
+    public static void main(String[] args) {
+        int c =3;
+        c=1;
+        c=2;
+        c=c+1;
+        int x =1;
+        int y = x*2+45*x;
+
+        String name ="Evgeny ";// name is a student's name
+        //
+        String number ="1";
+        String result =name+number;
+        System.out.println(result);
+
+        System.out.println(c);
+
+      /*  System.out.println("I will be  a programmer");
+        System.out.println("I will be  a programmer");
+        System.out.println("I will be  a programmer");*/
+
+        // 1573
+        // 1573 = 3*10^0 +7*10^1+5*10°2 +1*10°3
+
+        //0 0
+        //1 1
+        //2 10
+        //0*2°0 +1*2°1 =2
+
+        // 101101 = 1*2°5 +0*2°4+1*2°3+1*2°2+0*2°1+1*2°0=32+0+8+4+0+1=45
+        // 543210
+
+
+    }
+
+}
